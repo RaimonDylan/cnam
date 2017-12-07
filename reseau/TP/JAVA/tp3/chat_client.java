41a3210d623ccc3726ef7f861184865d02f18baf
utilisateur <utilisateur@classmob-ftlv02.univ-tln.fr>
2017-12-07 15:52:03 +0100

    fin exercice JAVA TP3

---
+++ reseau/TP/JAVA/tp3/chat_client.java
@@ -0,0 +1,62 @@
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.*;
+
+import java.rmi.*;
+
+interface ServeurTchatInterface extends Remote{
+    public void enregistrementClient(ClientTchatInterface client) throws RemoteException;
+    public void desenregistrementClient(ClientTchatInterface client) throws RemoteException;
+    public int nbClientsEnCours() throws RemoteException;
+    public void transfertMessage(String msg) throws RemoteException;
+}
+
+interface ClientTchatInterface extends Remote{
+       public void recuperationNouveauMessage(String msg) throws RemoteException;
+}
+
+class ClientTchat extends UnicastRemoteObject implements ClientTchatInterface  {
+
+    public ClientTchat()throws RemoteException{
+        super();
+    }
+
+    /* Méthode recuperationNouveauMessage(...)  qui affiche un message reçu depuis le serveur */
+    public void recuperationNouveauMessage(String msg){
+        System.out.println(msg);
+    }
+
+    public static void main(String args[]){
+        try {
+
+            if (args.length<2){
+                System.out.println("Nom d'hôte puis pseudo en ligne de commande SVP");
+                return;
+            }
+            String nom_hote=args[0];
+            String pseudo=args[1];
+
+            ServeurTchatInterface serveurTchatInterface = (ServeurTchatInterface)Naming.lookup("rmi://"+nom_hote+":1099/tchat"); 
+          
+            /* Création d'un objet de la classe courante et enregistrement de celui-ci auprès du serveur */  
+            ClientTchatInterface leClient = new ClientTchat(); 
+            serveurTchatInterface.enregistrementClient(leClient);
+
+            System.out.println (serveurTchatInterface.nbClientsEnCours());
+            System.out.println ("Discussion (-1 pour la quitter) : ");
+            while(true){
+                Scanner keyboard = new Scanner(System.in);
+                String s=keyboard.nextLine();
+                if(s.equals("-1")){
+                    serveurTchatInterface.desenregistrementClient(leClient);
+                    System.exit(0);
+                }
+                String message = pseudo+" : "+s;
+                serveurTchatInterface.transfertMessage(message);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ reseau/TP/JAVA/tp3/chat_serveur.java
@@ -0,0 +1,68 @@
+import java.rmi.*;
+import java.rmi.server.*;
+import java.net.*;
+import java.util.*;
+
+interface ServeurTchatInterface extends Remote{
+    public void enregistrementClient(ClientTchatInterface client) throws RemoteException;
+    public void desenregistrementClient(ClientTchatInterface client) throws RemoteException;
+    public int nbClientsEnCours() throws RemoteException;
+    public void transfertMessage(String msg) throws RemoteException;
+}
+
+interface ClientTchatInterface extends Remote{
+    public void recuperationNouveauMessage(String msg) throws RemoteException;
+}
+
+
+
+class ServeurTchat extends UnicastRemoteObject implements ServeurTchatInterface{
+
+    Vector<ClientTchatInterface> clients=new Vector<ClientTchatInterface>();
+
+    public ServeurTchat () throws RemoteException {
+        super();
+    }
+
+     /* en-tête de la méthode enregistrementClient(...) */
+    public void enregistrementClient(ClientTchatInterface client) throws RemoteException{
+        clients.add(client);
+    }
+    
+
+     /* en-tête de la méthode desenregistrementClient(...) */
+     public void desenregistrementClient(ClientTchatInterface client) throws RemoteException{
+        for (int i=0; i<clients.size(); i++)
+            if (client.equals((ClientTchatInterface)(clients.get(i))))
+                clients.removeElementAt(i);
+    }
+
+    /* retourne le nombre de clients en cours */
+    public int nbClientsEnCours() throws RemoteException {
+        return clients.size();
+    }
+
+    /* En-tête de la méthode transfertMessage(...) */
+    public void transfertMessage(String msg) throws RemoteException{
+            System.out.println(msg);
+            for (int i=0; i<clients.size(); i++){
+            	ClientTchatInterface client= (ClientTchatInterface)(clients.get(i));
+            	client.recuperationNouveauMessage(msg);
+        	}
+
+    }
+
+    public static void main(String[] args) {
+        try {
+
+            java.rmi.registry.LocateRegistry.createRegistry(1099);
+            ServeurTchat serveurTchat = new ServeurTchat();
+            String url="rmi://"+InetAddress.getLocalHost().getHostAddress()+"/tchat";
+            /* On enregistre dans la rmiregistry un objet de la classe courante */ 
+            Naming.rebind(url, serveurTchat);            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
