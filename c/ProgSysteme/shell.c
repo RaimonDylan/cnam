af168dea1a856e52f7ad6cfe52181d274d9165d5
RaimonDylan <dylanraimon@gmail.com>
2018-05-24 09:18:18 +0200

    ajout prog systeme

--- c/ProgSysteme/shell.c
+++ c/ProgSysteme/shell.c
@@ -9,18 +9,18 @@ int main(){
   pid_t pid,pidfils;
   int retour;
   char buffer[1024];
-  while(strcasecmp(buffer,"exit") != 0){
+  while(1){
     printf("[prompt]> ");
     fgets(buffer,1024,stdin);
-    if(buffer[strlen(buffer)-1] == '\n')
-      buffer[strlen(buffer)-1] = '\0';
+    if(buffer[strlen(buffer)-1] == '\n')buffer[strlen(buffer)-1] = '\0';
+    if(strcmp(buffer,"exit") == 0){printf("Adieu ...\n");exit(0);}
     switch(pid = fork()){
       case (pid_t)-1 :
         perror("naufrage ..");
         exit(2);
       case (pid_t)0 :
         execlp(buffer,buffer,NULL);
-        if(strcasecmp(buffer,"exit") != 0)fprintf(stderr, "Commande inconnue ...\n");
+        fprintf(stderr, "Commande inconnue ...\n");
         exit(1);
       default:
         pidfils = wait(&retour);
