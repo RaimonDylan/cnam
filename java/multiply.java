50e71b3dd7e49727d056da64e6d398d7f4c5bf04
RAIMON Dylan <dylanraimon@gmail.com>
2018-09-09 21:04:11 +0200

    Update multiply.java

--- java/multiply.java
+++ java/multiply.java
@@ -1,11 +1,21 @@
-public static int[][] multiply(int[][] a, int[][] b) throws Exception{
+// one class needs to have a main() method
+public class Multiply
+{
+   public static void main(String[] args)
+  {
+    int[][] a = {{1,2},{3,4}};
+	int[][] b = {{4,3},{2,1}};
+    int[][] c = multiply(a,b);
+    afficher(c);
+  }
+  public static int[][] multiply(int[][] a, int[][] b){
 		if(a[0].length != b.length){
-			throw new Exception("impossible !");
+			return null;
 		}
 		int l = (a.length * a[0].length < b.length * b[0].length)?b.length:a.length;
 		int c = (a.length * a[0].length < b.length * b[0].length)?b[0].length:a[0].length;
 	 
-		int[l][c] result;
+		int[][] result =  new int[l][c];
 		 
 		 l = 0;
 	     for (int i = 0;i < a.length;i++){ 
@@ -22,3 +32,16 @@ public static int[][] multiply(int[][] a, int[][] b) throws Exception{
 	     }
 		return result;
 	}
+  public static void afficher(int [][]M){
+
+		    for (int i = 0;i < M.length;i++)
+		    {
+		        for (int j = 0;j < M[0].length ;j++)
+		        {
+		            System.out.printf("%4d ",M[i][j] );
+		        }
+		       System.out.println();
+		    }
+		    System.out.println();
+	}
+}
