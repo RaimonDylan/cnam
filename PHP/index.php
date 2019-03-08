f4f1272095288e99fe49219ed42287e61c36ee5c
David MOLINARI <david@rmolinari.net>
2019-03-08 08:08:17 +0100

    y

---
+++ PHP/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Red Strings Club</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="icon" type="image/png" href="images/logo.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy" rel="stylesheet">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/parallax.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<html><body>
+<form method="post" action="verif.php">
+    Nom : <input type="text" name="nom" size="12"><br>
+    Prénom : <input type="text" name="prenom" size="12">
+    <input type="submit" value="OK">
+</form></body></html>
+
+
+
+</body>
+</html>
+
---
+++ PHP/verif.php
@@ -0,0 +1,4 @@
+<?php
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+print("<h2>Bonjour $prenom $nom</h2>");
